fd2d5579e8503f98915d6020c5b33b8a8bc865e1
Joseph Haaga <haaga.joe@gmail.com>
2017-05-27 15:42:27 -0400

    control flow basics

---
+++ ControlFlow.lisp
@@ -0,0 +1,12 @@
+(defun if-statement() 
+		(if (oddp 5)
+			'odd-numbered
+			(/ 1 0)
+		)
+)
+
+; (defun run ()
+; 	(if (oddp 5) 
+; 		'odd-number
+; 		(/ 1 0)) 
+; )
---
+++ Lists.lisp
@@ -0,0 +1,8 @@
+(defun run() 
+		(defparameter z (cons 'n (cons 'o 'p)))
+		(car z)
+		; (cdr z)
+		; (cadr z)
+	
+)
+
