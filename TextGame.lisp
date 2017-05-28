fa57b3b1925ddcea4fa81f3334784bcb8f2976d6
Joseph Haaga <haaga.joe@gmail.com>
2017-05-28 11:22:13 -0400

    more TextGame stuff

--- TextGame.lisp
+++ TextGame.lisp
@@ -5,8 +5,29 @@
  		(attic (you are in the attic.
 			there is a giant welding torch in the corner.))))
 
+(defparameter *edges* '((living-room (garden west door) (attic upstairs ladder))
+ 		(garden (living-room east door))
+ 		(attic (living-room downstairs ladder))))
+
+(defparameter *objects* '(whiskey bucket chain frog))
+
+(defparameter *object-locations* '((whiskey living-room) (bucket living-room) (chain garden) (frog garden)) )
 ; Pass *nodes* as a parameter to cohere to functional programming style
 ; 	- functions only operate on parameters & return a value; easier to test in isolation
 (defun describe-location(location nodes)
 	(cadr (assoc location nodes))
+)
+
+(defun describe-path(edge)
+	`(There is a ,(caddr edge) going ,(cadr edge) from here)
+)
+
+(defun describe-paths(location edges)
+	(apply #'append (mapcar #'describe-path (cdr (assoc location edges))))
+)
+
+(defun objects-at (loc objs obj-locs)
+	(labels ((at-loc-p (obj)
+			 (eq (cadr (assoc obj obj-locs)) loc)))
+		(remove-if-not #'at-loc-p objs))
 )
